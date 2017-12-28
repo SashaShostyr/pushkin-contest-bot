5f5c8320a89a4eef6e35faa2a9e87ffb8f23a49e
Sasha Shostyr <sashashostyr@gmail.com>
2017-12-28 14:14:36 +0300

    Fix level-1.3

--- config/initializers/records.rb
+++ config/initializers/records.rb
@@ -8,32 +8,32 @@ end
 file = File.read(File.expand_path("./db/poems-full.json"))
 data = JSON.parse(file)
 
-DATA_LEVEL1 = {}
+DATA1 = {}
 data.each do |poem|
   poem[1].each do |line|
     if poem[0].match(/[\*]/).nil?
-      DATA_LEVEL1[delete_punctuation(line)] = poem[0]
+      DATA1[delete_punctuation(line)] = poem[0]
     elsif
-      DATA_LEVEL1[delete_punctuation(line)] = poem[1][0]
+      DATA1[delete_punctuation(line)] = poem[1][0]
     end
   end
 end
 File.open('db/level1.json', 'w') do |f|
-  f.write(DATA_LEVEL1.to_json)
+  f.write(DATA1.to_json)
 end
 
-DATA_LEVEL2 = {}
+DATA2 = {}
 data_all_lines = data.map {|d| d[1]} 
 data_all_lines.flatten.uniq.each do |line|
   words = delete_punctuation(line).split(' ')
   words.each do |word|
     new_line = line.sub(/#{word}/, '%WORD%')
-    DATA_LEVEL2[new_line] = word
+    DATA2[new_line] = word
   end
 end
 
 File.open('db/level2.json', 'w') do |f|
-  f.write(DATA_LEVEL2.to_json)
+  f.write(DATA2.to_json)
 end
 
 DATA_LEVEL1 = JSON.parse File.read(File.expand_path("./db/level1.json"))
