d68977fffdbcfa27bd901d15478071de54048bd1
Sasha Shostyr <sashashostyr@gmail.com>
2017-12-28 15:58:22 +0300

    Add level-3

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -20,6 +20,8 @@ class QuizController < ApplicationController
       solve_level1
     when 2
       solve_level2
+    when 3
+      solve_level3
     end
   end
 
@@ -29,6 +31,15 @@ class QuizController < ApplicationController
 
   def solve_level2
     DATA_LEVEL2[delete_punctuation(@question)]
+  end
+
+  def solve_level3
+    lines = @question.split("\n")
+    answers = []
+    lines.each do |line|
+      answers << DATA_LEVEL2[delete_punctuation(line)]
+    end
+    answers.join(',')
   end
 
   def quiz_params
