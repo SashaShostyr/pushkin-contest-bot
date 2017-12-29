ebd30cc786d54fcf1857270593b0eb54ffce80b3
Sasha Shostyr <sashashostyr@gmail.com>
2017-12-29 15:09:36 +0300

    Fix level-8.2

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -71,14 +71,14 @@ class QuizController < ApplicationController
 
   def solve_level8
     answer = ''
-    chars_all = delete_punctuation(@question).tr(' ','').chars.sort
-    chars_all.each_with_index do |char, index|
-      chars_all[index] = ''
-      answer =  DATA_LEVEL8[chars_all.join] unless DATA_LEVEL8[chars_all.join].nil?
-      chars_all[index] = char
-    end
-    #key = FuzzyMatch.new(DATA_LEVEL6.keys).find(delete_punctuation(@question).tr(' ', '').chars.sort.join)
-    #answer = DATA_LEVEL6[key]
+    #chars_all = delete_punctuation(@question).tr(' ','').chars.sort
+    #chars_all.each_with_index do |char, index|
+      #chars_all[index] = ''
+      #answer =  DATA_LEVEL8[chars_all.join] unless DATA_LEVEL8[chars_all.join].nil?
+      #chars_all[index] = char
+    #end
+    key = FuzzyMatch.new(DATA_LEVEL6.keys).find(delete_punctuation(@question).tr(' ', '').chars.sort.join)
+    answer = DATA_LEVEL6[key]
     answer
   end
 
